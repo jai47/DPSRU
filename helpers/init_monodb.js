71cb343a7cd6e44b7d9a1340b2106f0e1d66fd67
Jai <official.jaimishra@gmail.com>
2023-01-29 17:41:44 +0530

    more changes

--- helpers/init_monodb.js
+++ helpers/init_monodb.js
@@ -2,5 +2,5 @@ const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
 main().catch(err => console.log(err));
 async function main() {
-    await mongoose.connect('mongodb+srv://<username>:<password>@cluster0.cmv7njg.mongodb.net/test');
+    await mongoose.connect('mongodb+srv://jaimishra2004:7october2004@cluster0.cmv7njg.mongodb.net/test');
 }
